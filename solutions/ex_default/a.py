d2c5c2f3216b15d715dde71112a994cdc54f59a0
Marek Wawreniuk <marek.wawreniuk@websensa.com>
2021-12-07 15:04:56 +0100

    feat: add modify default a and b

--- solutions/ex_default/a.py
+++ solutions/ex_default/a.py
@@ -1,5 +1,15 @@
+from typing import List
+
 from line import Line
 from utils.readlines import read_lines
 
 lines = read_lines(Line)
-print()
+
+
+def get_solution(_lines: List[Line]) -> str:
+    print(_lines[0])
+    solution = 0
+    return str(solution)
+
+
+print(get_solution(lines))
--- solutions/ex_default/b.py
+++ solutions/ex_default/b.py
@@ -1,3 +1,15 @@
-def readlines():
-    f = open("in")
-    return [int(lines.rstrip()) for lines in f]
+from typing import List
+
+from line import Line
+from utils.readlines import read_lines
+
+lines = read_lines(Line)
+
+
+def get_solution(_lines: List[Line]) -> str:
+    print(_lines[0])
+    solution = 0
+    return str(solution)
+
+
+print(get_solution(lines))
